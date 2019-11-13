987432b2da20de2e4662dd84988d6341ae7e24b4
Knut Bjørnar Wålberg <Knut.Bjornar.Walberg@nav.no>
2019-11-13 16:38:56 +0100

    MELOSYS-3339_fatte_korrigert_vedtak

--- scripts/test/saksflyt/vedtak/index.js
+++ scripts/test/saksflyt/vedtak/index.js
@@ -1,7 +1,9 @@
 const { endre } = require('./endre');
 const { fatt } = require('./fatt');
+const { revurder } = require('./revurder');
 
 module.exports.vedtak = {
   endre,
   fatt,
+  revurder,
 };
