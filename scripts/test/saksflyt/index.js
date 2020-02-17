5f9b3d460ec0e7b2cc35aa495d4fd592193857b6
JohanSakshaug <johan.sakshauger@nav.no>
2020-02-17 17:06:27 +0100

    Fjern død kode

--- scripts/test/saksflyt/index.js
+++ scripts/test/saksflyt/index.js
@@ -1,12 +1,10 @@
 const { anmodningsperioder } = require('./anmodningsperioder');
-const { soknader } = require('./soknader');
 const { utpekingavvis } = require('./utpekingavvis');
 const { unntaksperioder } = require('./unntaksperioder');
 const { vedtak } = require('./vedtak');
 
 module.exports = {
   anmodningsperioder,
-  soknader,
   vedtak,
   utpekingavvis,
   unntaksperioder,
