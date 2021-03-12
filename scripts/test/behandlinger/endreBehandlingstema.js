13a33619981403fe6bf9a537da471ec623018852
martonSkjevelandNav <marton.skjeveland@nav.no>
2021-03-12 16:17:07 +0100

    Oppdaterte feil fil i forrige commit.. Byttet til status-filen

--- scripts/test/behandlinger/endreBehandlingstema.js
+++ scripts/test/behandlinger/endreBehandlingstema.js
@@ -3,7 +3,6 @@ const Schema = require('../../utils/schema-util');
 
 const testAll = navn => {
   Schema.printWhiteText(navn);
-  Schema.testPostMockFiles(navn);
   Schema.testGetMockFiles(navn);
 };
 
--- scripts/test/behandlinger/status.js
+++ scripts/test/behandlinger/status.js
@@ -3,6 +3,7 @@ const Schema = require('../../utils/schema-util');
 
 const testAll = navn => {
   Schema.printWhiteText(navn);
+  Schema.testPostMockFiles(navn);
   Schema.testGetMockFiles(navn);
 };
 
